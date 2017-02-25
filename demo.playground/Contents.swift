c6fbf50ca51fc4c3413a2536bbea6d45797cf55c
Tungdt <daotung253@gmail.com>
2017-02-25 10:24:55 +0700

    test local develop

---
+++ demo.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a:Int!
+var b:Int?
+
+print(b)
+
+struct connguoi{
+    var tenNguoi:String?
+    var cmnd:String!
+}
+var a1:String?
+a1 ="abc"
+print(a1)
